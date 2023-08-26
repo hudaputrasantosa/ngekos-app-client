8247bf387ff0b30a9e314c35eaee00898f57dddb
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-27 01:28:01 +0700

    [add] model kos

---
+++ api/models/Kos.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const KosSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+  cheapestPrice: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("Kos", KosSchema);
