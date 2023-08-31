c5372364cd535b5b21942b68244579dae421b045
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-31 13:30:12 +0700

    [update] remove mailList and change testimoniList

---
+++ client/src/components/testimoniList/TestimoniList.jsx
@@ -0,0 +1,46 @@
+import "./testimoniList.css"
+
+const TestimoniList = () => {
+  return (
+<div class="carousel-container">
+  <div class="carousel">
+    <div class="card">
+      <div class="avatar">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="Customer Photo"/>
+
+      </div>
+      <div class="card-content">
+        <h3 class="card-title">John Doe</h3>
+        <p class="card-position">CEO, ABC Company</p>
+        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel metus eget libero euismod bibendum.</p>
+      </div>
+    </div>
+        <div class="card">
+      <div class="avatar">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="Customer Photo"/>
+
+      </div>
+      <div class="card-content">
+        <h3 class="card-title">John Doe</h3>
+        <p class="card-position">CEO, ABC Company</p>
+        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel metus eget libero euismod bibendum.</p>
+      </div>
+    </div>
+        <div class="card">
+      <div class="avatar">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" alt="Customer Photo"/>
+
+      </div>
+      <div class="card-content">
+        <h3 class="card-title">John Doe</h3>
+        <p class="card-position">CEO, ABC Company</p>
+        <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel metus eget libero euismod bibendum.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+  )
+}
+
+export default TestimoniList
