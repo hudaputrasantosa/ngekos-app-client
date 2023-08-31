d2b3ca2e54f99f4a4d5233284c0bbba181f87b08
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-31 13:32:30 +0700

    [update] header and navbar for searching kos

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -21,7 +21,7 @@ const Header = ({ type }) => {
       key: "selection",
     },
   ]);
-  const [openOptions, setOpenOptions] = useState(false);
+  // const [openOptions, setOpenOptions] = useState(false);
   const [options, setOptions] = useState({
     adult: 1,
     children: 0,
@@ -30,14 +30,14 @@ const Header = ({ type }) => {
 
   const navigate = useNavigate();
 
-  const handleOption = (name, operation) => {
-    setOptions((prev) => {
-      return {
-        ...prev,
-        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
-      };
-    });
-  };
+  // const handleOption = (name, operation) => {
+  //   setOptions((prev) => {
+  //     return {
+  //       ...prev,
+  //       [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+  //     };
+  //   });
+  // };
 
   const handleSearch = () => {
     navigate("/hotels", { state: { destination, date, options } });
@@ -53,11 +53,10 @@ const Header = ({ type }) => {
         {type !== "list" && (
           <>
             <h1 className="headerTitle">
-              A lifetime of discounts? It's Genius.
+              Cari Kos dengan 1 Klik? itu Mudah.
             </h1>
             <p className="headerDesc">
-              Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free Lamabooking account
+              Dapatkan informasi lengkap tentang sewa Kos dengan 1 kali Klik saja, hanya di Ngekoss.id
             </p>
             
             <div className="headerSearch">
@@ -65,12 +64,12 @@ const Header = ({ type }) => {
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
                 <input
                   type="text"
-                  placeholder="Where are you going?"
+                  placeholder="Mau cari info sewa Kos dimana?"
                   className="headerSearchInput"
                   onChange={(e) => setDestination(e.target.value)}
                 />
               </div>
-              <div className="headerSearchItem">
+              {/* <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
                 <span
                   onClick={() => setOpenDate(!openDate)}
@@ -89,7 +88,7 @@ const Header = ({ type }) => {
                     minDate={new Date()}
                   />
                 )}
-              </div>
+              </div> */}
               {/* <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faPerson} className="headerIcon" />
                 <span
@@ -166,7 +165,7 @@ const Header = ({ type }) => {
               </div> */}
               <div className="headerSearchItem">
                 <button className="headerBtn" onClick={handleSearch}>
-                  Search
+                  Cari Kos Sekarang!
                 </button>
               </div>
             </div>
