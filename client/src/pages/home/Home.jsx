9411b6ffccaafe557c08f14ad692fbb608dfe29d
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-09-01 23:48:31 +0700

    [update]

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import TestimoniList from "../../components/testimoniList/TestimoniList";
-import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
