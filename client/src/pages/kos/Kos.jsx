01adf3b35cbd013664dbe7888752122799acaf14
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-11-11 21:35:59 +0700

    [update] fix bug blank white

--- client/src/pages/kos/Kos.jsx
+++ client/src/pages/kos/Kos.jsx
@@ -15,32 +15,9 @@ import { useLocation } from "react-router-dom";
 
 const Kos = () => {
   const id = useLocation().pathname.split("/")[2];
-  const [slideNumber, setSlideNumber] = useState(0);
+   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
   const {data, loading, error, reFetch} = useFetch(`/koses/find/${id}`);
-
-  // console.log(data);
-
-  const photos = [
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
-    },
-    {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
-    },
-  ];
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -63,7 +40,9 @@ const Kos = () => {
     <div>
       <Navbar />
       <Header type="list" />
-      { loading ? ("Loading, Please wait..") : (<div className="hotelContainer">
+      { loading? ("Loading.. please wait") : (
+
+      <div className="hotelContainer">
         {open && (
           <div className="slider">
             <FontAwesomeIcon
@@ -87,15 +66,15 @@ const Kos = () => {
           </div>
         )}
         <div className="hotelWrapper">
-          <h1 className="hotelTitle">{data.name}s</h1>
+          <h1 className="hotelTitle">{data?.name}</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
-            <span>{data.address}, {data.city}</span>
+            <span>{data?.address}, {data?.city}</span>
           </div>
           <span className="hotelDistance">
             Excellent location – 500m from center
           </span>
-          <div className="hotelImages">
+           <div className="hotelImages">
             {data.photos.map((photo, i) => (
               <div className="hotelImgWrapper" key={i}>
                 <img
@@ -111,24 +90,26 @@ const Kos = () => {
             <div className="hotelDetailsTexts">
               <h1 className="hotelTitle">Deskripsi</h1>
               <p className="hotelDesc">
-               {data.description}
+               {data?.description}
               </p>
             </div>
             <div className="hotelDetailsPrice">
-              <h1>Sisa {data.stock} Kamar</h1>
+              <h1>Sisa {data?.stock} Kamar</h1>
               <div>
                 <span>Fasilitas :</span>
-                <p> { (data.facility.length > 0) ? data.facility.join(', ') : "Tidak ditampilkan"}</p>
+                <p> { (data?.facility.length > 0) ? data.facility.join(', ') : "Tidak ditampilkan"}</p>
               </div>
               <h2>
-                <b>{(data.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }).substring(0, (data.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }).length - 3 )}</b> / Bulan
+                {/* this problem (toLocaleString) */}
+                {/* <b>{(data?.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }).substring(0, (data.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }).length - 3 )}</b>  */}
+                { data?.price} / Bulan
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
           </div>
         </div>
-        <Footer />
       </div>)}
+        <Footer />
     </div>
   );
 };
