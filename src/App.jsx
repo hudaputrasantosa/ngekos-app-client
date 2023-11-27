dc355195dd659fa5fc1af64b2f57e6d7c5d2b6ed
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-11-27 22:59:06 +0700

    [update] feat: linked login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Kos from "./pages/kos/Kos";
 import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/koses" element={<List />} />
         <Route path="/kos/:id" element={<Kos />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import "./navbar.css"
-import {Link} from "react-router-dom"
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{ color:"inherit", textDecoration: "none" }}>
-        <span className="logo">Ngekoss.id</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Ngekoss.id</span>
         </Link>
         <div className="navItems">
           <button className="navButton">Daftar</button>
-          <button className="navButton">Masuk</button>
+          <Link to="/login">
+            <button className="navButton">Masuk</button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
